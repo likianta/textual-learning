54da1e5a1763115c85cc25c9428fb776d3442bef
likianta <likianta@foxmail.com>
2022-01-25 16:26:49 +0800

    Add demo

---
+++ src/easy_launcher.py
@@ -0,0 +1,6 @@
+"""
+automatically add paths to sys.path then launch target file.
+
+cmd:
+    python3 easy_launcher.py <target_py_file>
+"""
---
+++ src/template/app.py
@@ -0,0 +1,9 @@
+from textual.app import App
+
+
+class MyApp(App):
+    async def on_mount(self):
+        pass
+    
+    
+MyApp.run()
