d5c76d5ab883e489c4973606ca74b25af7c6af3c
likianta <likianta@foxmail.com>
2022-01-28 00:34:48 +0800

    Update app template

--- src/template/app.py
+++ src/template/app.py
@@ -1,9 +1,16 @@
 from textual.app import App
+from textual.widgets import Placeholder
+
+from textual_extensions import event_bus
 
 
 class MyApp(App):
+    
     async def on_mount(self):
-        pass
-    
-    
+        await self.view.dock(Placeholder(), edge='left', size=40)
+        await self.view.dock(Placeholder(), edge='bottom', size=3)
+        await self.view.dock(Placeholder(), edge='top')
+
+
 MyApp.run()
+event_bus.close()
