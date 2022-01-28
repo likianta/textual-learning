e7a1250a062a6c0c68ce57bc978ec93da41eb5c4
likianta <likianta@foxmail.com>
2022-01-28 16:26:43 +0800

    Button adds `_enabled` attribute

--- src/textual_extensions/widgets/button.py
+++ src/textual_extensions/widgets/button.py
@@ -6,6 +6,7 @@ class Button(Widget):
     
     def __init__(self, label: str, padding=1, width=None):
         super().__init__()
+        self._enabled = True
         self._label = label
         # self._padding = padding
         self._pressed = False
@@ -16,17 +17,28 @@ class Button(Widget):
         return self._width
     
     def render(self) -> RenderableType:
-        # normal color: light blue on gray
-        # pressed color: light blue on dark magenta
         label = self._label
         str_spacing = ' ' * ((self._width - len(label)) // 2)
         label = str_spacing + label + str_spacing
-        label = '[{fg} on {bg}]{label}[/]'.format(
-            fg='color(87)',
-            bg='#444444' if self._pressed else '#3F298F',
-            label=label
-        )
-        return label
+        if self._enabled:
+            # normal color: light blue on gray
+            # pressed color: light blue on dark magenta
+            return '[{fg} on {bg}]{label}[/]'.format(
+                fg='color(87)',
+                bg='#444444' if self._pressed else '#3F298F',
+                label=label
+            )
+        else:
+            return '[{fg} on {bg}]{label}[/]'.format(
+                fg='color(8)',
+                bg='#444444',
+                label=label
+            )
+    
+    def set_enabled(self, value: bool):
+        if self._enabled != value:
+            self._enabled = value
+            self.refresh()
     
     async def on_mouse_down(self, event):
         self._pressed = True
